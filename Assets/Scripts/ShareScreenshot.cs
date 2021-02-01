52a76ccdba40946b15847129cd5fe36a735ed6ed
Maxime Mohandi <mohandi.maxime@gmail.com>
2021-02-01 19:47:01 +0100

    initial commit

---
+++ Assets/Scripts/ShareScreenshot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ShareScreenshot : MonoBehaviour
+{
+    public void Share()
+    {
+        StartCoroutine(ScreenAndShare());
+    }
+
+    private IEnumerator ScreenAndShare()
+    {
+        yield return new WaitForEndOfFrame();
+
+        // Take a screenshot
+        Texture2D screenShot = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        screenShot.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        screenShot.Apply();
+
+        string screenName = "screenshot_" + DateTime.Now.ToString("ddmmyyhhMMss") + ".png";
+        string filePath = Path.Combine(Application.temporaryCachePath, screenName);
+        File.WriteAllBytes(filePath, screenShot.EncodeToPNG());
+
+        Destroy(screenShot);
+
+        // Share 
+        new NativeShare()
+            .AddFile(filePath)
+            .SetSubject("Cerealis Coloring AR")
+            .SetText("#cerealis #coloring #AR")
+            .Share();
+
+    }
+}
---
+++ Assets/Scripts/opacityPlane.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opacityPlane : MonoBehaviour
+{
+    public float opacity;
+
+    // Start is called before the first frame update
+    void Start() {
+        Material planeMaterial = this.GetComponent<MeshRenderer>().material;
+        Color planeColor = planeMaterial.color;
+        planeColor.a = opacity;
+        planeMaterial.color = planeColor;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
